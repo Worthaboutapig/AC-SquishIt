0e2c7d313439f985a1482cadd3203521dbf46a0a
Worthaboutapig <jeremy@savetrees.co.uk>
2016-01-05 08:59:09 +0000

    Removed dependency on System.Web from IMachineConfigReader by creating custom private class for System.Web.DeploymentSection, added XML comments to classes etc.

--- SquishIt.Framework/Utilities/MachineConfigReader.cs
+++ SquishIt.Framework/Utilities/MachineConfigReader.cs
@@ -1,33 +1,59 @@
 ﻿using System.Configuration;
-using System.Web.Configuration;
 
 namespace SquishIt.Framework.Utilities
 {
-    public interface IMachineConfigReader
-    {
-        bool IsNotRetailDeployment { get; }
-    }
+	/// <summary>
+	/// Maps properties on the machine.config file
+	/// </summary>
+	public interface IMachineConfigReader
+	{
+		/// <summary>
+		/// Whether the machine.config is not configured for retail deployment
+		/// </summary>
+		bool IsNotRetailDeployment { get; }
+	}
 
-    class MachineConfigReader : IMachineConfigReader
-    {
-        public bool IsNotRetailDeployment
-        {
-            get
-            {
-                //check retail setting in machine.config
-                //Thanks Dave Ward! http://www.encosia.com
-                System.Configuration.Configuration machineConfig = ConfigurationManager.OpenMachineConfiguration();
-                var group = machineConfig.GetSectionGroup("system.web");
-                if(group != null)
-                {
-                    var appSettingSection = (DeploymentSection)group.Sections["deployment"];
-                    if(appSettingSection.Retail)
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-    }
+	/// <summary>
+	/// Reads values from the machine.config
+	/// </summary>
+	class MachineConfigReader : IMachineConfigReader
+	{
+		/// <summary>
+		/// Whether the machine.config is not configured for retail deployment
+		/// </summary>
+		public bool IsNotRetailDeployment
+		{
+			get
+			{
+				//check retail setting in machine.config
+				//Thanks Dave Ward! http://www.encosia.com
+				var machineConfig = ConfigurationManager.OpenMachineConfiguration();
+				var systemWebSection = machineConfig.GetSectionGroup("system.web");
+
+				if (systemWebSection == null)
+				{
+					return true;
+				}
+
+				var deploymentSection = (DeploymentSection)systemWebSection.Sections["deployment"];
+
+				return !deploymentSection.Retail;
+			}
+		}
+
+		/// <summary>
+		/// Extracts the value of 'Retail' from the config file, without the System.Web dependency
+		/// </summary>
+		private class DeploymentSection : ConfigurationSection
+		{
+			/// <summary>
+			/// Gets or sets a value that specifies whether Web applications on the computer are deployed in retail mode.
+			/// </summary>
+			/// <returns>
+			/// true if Web applications are deployed in retail mode; otherwise, false. The default is false.
+			/// </returns>
+			[ConfigurationProperty("retail", DefaultValue = false)]
+			public bool Retail { get; set; }
+		}
+	}
 }
